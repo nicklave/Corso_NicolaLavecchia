bdf0a0faac72dd6f274e48094387bf1d7f52386d
nicklave <nicola.lavecchia43@gmail.com>
2024-08-29 23:36:36 +0200

    Esercizi Python

--- 29-08/29_08_Ex5.py
+++ 29-08/29_08_Ex5.py
@@ -4,15 +4,24 @@
 #  Altrimenti, stampa "Il numero non è primo". si ferma il
 #  tutto quando ha 5 numeri primi
 
-x = int(input("Inserisci un numero: "))
 
-b = True
-for i in range(2,x+1):
-    if x/i != 1:
-        b = False
-if b == True:
-    print(x, "è un numero primo")
-else:
-    print(x,"non è un numero primo")
+count = 0
+l = []
+while count < 5:
+    x = int(input("Inserisci un numero: "))
+    b = True
+    for i in range(2,x):
+        resto = x - (x/i)
+        if resto.is_integer() :
+            b = False
+    if b == True:
+        print(x," è un numero primo")
+        l.append(x)
+        count+=1
+    else:
+        print(x," non è un numero primo")
+   
+if count == 5:
+    print("Hai trovato 5 numeri primi: ",l)
 
 

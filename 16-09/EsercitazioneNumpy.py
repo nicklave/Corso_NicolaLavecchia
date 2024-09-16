214250320c7cfd448d8c9d17ff75345110ce138f
nicklave <nicola.lavecchia43@gmail.com>
2024-09-16 15:14:29 +0200

    Update EsercitazioneNumpy.py

--- 16-09/EsercitazioneNumpy.py
+++ 16-09/EsercitazioneNumpy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 #  Utilizza la funzione np.arange per creare
 #  un array di numeri interi da 10 a 49.
@@ -11,40 +12,41 @@ import numpy as np
 
 a = np.arange(10,50)
 
-print(a.dtype)
+print(f"Tipo iniziale dell'array: {a.dtype}")
 
 a.dtype = 'float64'
 
-print(a.dtype)
+print("Tipo modificato dell'array: ",a.dtype)
 
-print(a.shape)
+print("Forma dell'array: ",a.shape)
 
 
 # SLICING E INDEXING
 
 array = np.random.randint(10,50,size = 20)
-print(array)
+print(f"Array originale: {array}")
 
 #estraggo i primi 10 elementi
 
-print(array[:11])
+print(f"Primi 10 elementi: {array[:11]}")
 
 #estraggo gli ultimi 5 elementi
 
-print(array[-5:])
+print(f"Ultimi 5 elementi: {array[-5:]}")
 
 #da indice 5 a indice 15
 
-print(array[5:15])
+print(f"Da indice 5 a indice 15 {array[5:15]}")
 
 #ogni terzo elemento
 
-print(array[::3])
+print(f"Ogni terzo elemento: {array[::3]}")
 
 #modifico tramite slicing
+array2 = copy.copy(array)
 
 indici = np.array([5,6,7,8,9])
 
-array[indici] = 99 
+array2[indici] = 99 
 
-print(array)
+print(f"Array modificato tramite slicing: {array2}")

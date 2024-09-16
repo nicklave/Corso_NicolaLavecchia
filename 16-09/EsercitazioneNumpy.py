91eb502aa01be3cd3936aec96286c960c95ba955
nicklave <nicola.lavecchia43@gmail.com>
2024-09-16 15:55:02 +0200

    Update EsercitazioneNumpy.py

--- 16-09/EsercitazioneNumpy.py
+++ 16-09/EsercitazioneNumpy.py
@@ -8,37 +8,37 @@ import copy
 a = np.arange(10,50)
 
 #  Verifica il tipo di dato dell'array e stampa il risultato.
-print(f"Tipo iniziale dell'array: {a.dtype}")
+print(f"Tipo iniziale dell'array: {a.dtype}\n")
 
 #  Cambia il tipo di dato dell'array in float64 e verifica di nuovo il tipo di dato.
 a.dtype = 'float64'
 
-print("Tipo modificato dell'array: ",a.dtype)
+print("Tipo modificato dell'array: ",a.dtype,"\n")
 
 #  Stampa la forma dell'array.
-print("Forma dell'array: ",a.shape)
+print("Forma dell'array: ",a.shape,"\n")
 
 
 # SLICING E INDEXING
 
 array = np.random.randint(10,50,size = 20)
-print(f"Array originale: {array}")
+print(f"Array originale: {array}\n")
 
 #estraggo i primi 10 elementi
 
-print(f"Primi 10 elementi: {array[:11]}")
+print(f"Primi 10 elementi: {array[:11]}\n")
 
 #estraggo gli ultimi 5 elementi
 
-print(f"Ultimi 5 elementi: {array[-5:]}")
+print(f"Ultimi 5 elementi: {array[-5:]}\n")
 
 #da indice 5 a indice 15
 
-print(f"Da indice 5 a indice 15 {array[5:15]}")
+print(f"Da indice 5 a indice 15 {array[5:15]}\n")
 
 #ogni terzo elemento
 
-print(f"Ogni terzo elemento: {array[2:21:3]}")
+print(f"Ogni terzo elemento: {array[2:21:3]}\n")
 
 #modifico tramite slicing
 array2 = copy.copy(array)
@@ -50,6 +50,37 @@ indici = np.array([5,6,7,8,9])
 
 array2[indici] = 99 
 
-print(f"Array modificato tramite fancy indexing: {array2}")
+print(f"Array modificato tramite fancy indexing: {array2}\n")
 
-print(f"Array modificato tramite slicing: {array3}")
+print(f"Array modificato tramite slicing: {array3}\n")
+
+# OPERAZIONI SU ARRAY
+
+# Crea un array di 12 numeri equidistanti tra 0 e 1 usando linspace.
+
+ar = np.linspace(0,1,12)
+arr = np.round(ar,3)
+print(f"Array di 12 numeri equidistanti: {arr}\n")
+
+
+#  Cambia la forma dell'array a una matrice 3x4.
+
+arr.reshape(3,4)
+print(f"Array ridimensionato: {arr}\n")
+
+#  Genera una matrice 3x4 di numeri casuali tra 0 e 1.
+
+matr = np.random.rand(3,4)
+matrice = np.round(matr,3)
+
+print(f"Matrice 3x4:\n {matrice}\n")
+
+print(f"Matrice 4x3:\n {matrice.reshape(4,3)}\n")
+
+
+#  Calcola e stampa la somma degli elementi di entrambe le matrici.
+
+somma_arr = np.sum(arr)
+somma_matr = np.sum(matrice)
+
+print(f"La somma dell'array è {somma_arr}\nLa somma della matrice è {somma_matr}")

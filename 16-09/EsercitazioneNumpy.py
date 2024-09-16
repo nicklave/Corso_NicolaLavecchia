39bde1b7e3e03733a23e591502e853fcaaa1a07d
nicklave <nicola.lavecchia43@gmail.com>
2024-09-16 15:09:53 +0200

    Update EsercitazioneNumpy.py

--- 16-09/EsercitazioneNumpy.py
+++ 16-09/EsercitazioneNumpy.py
@@ -18,3 +18,33 @@ a.dtype = 'float64'
 print(a.dtype)
 
 print(a.shape)
+
+
+# SLICING E INDEXING
+
+array = np.random.randint(10,50,size = 20)
+print(array)
+
+#estraggo i primi 10 elementi
+
+print(array[:11])
+
+#estraggo gli ultimi 5 elementi
+
+print(array[-5:])
+
+#da indice 5 a indice 15
+
+print(array[5:15])
+
+#ogni terzo elemento
+
+print(array[::3])
+
+#modifico tramite slicing
+
+indici = np.array([5,6,7,8,9])
+
+array[indici] = 99 
+
+print(array)

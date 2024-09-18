74513925647796137d46bff903cabea287613d18
nicklave <nicola.lavecchia43@gmail.com>
2024-09-18 17:47:46 +0200

    Update Esercizio base3.py

--- Machine_Learning/Esercizio base3.py
+++ Machine_Learning/Esercizio base3.py
@@ -7,6 +7,7 @@ from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import mean_squared_error
+import pandas as pd
 
 data = load_wine()
 
@@ -20,9 +21,10 @@ class ModelloWine:
     
     def caricamento_dati(self):
 
-        data = load_wine()
-        self.X = data.data
-        self.y = data.target
+        self.data = load_wine()
+        self.X = self.data.data
+        self.y = self.data.target
+
         return self.X, self.y
 
     def standardizzazione(self):
@@ -44,31 +46,40 @@ class ModelloWine:
     
     def score(self):
         try:
-            accuracy = accuracy_score(self.y_test,self.predizione)
-            mse = mean_squared_error(self.y_test,self.predizione)
+            self.accuracy = accuracy_score(self.y_test,self.predizione)
+            self.mse = mean_squared_error(self.y_test,self.predizione)
 
-            print(f"L'accuratezza del modello è {accuracy}")
-            print(f"\nL'errore quadratico medio del modello è {mse}")
-
-            return accuracy,mse
+            return self.accuracy,self.mse
         except:
             print("Non hai ancora eseguito il modello!")
     
     def performance(self):
 
         try:
-            report = classification_report(self.y_test,self.predizione)
-            matrice = confusion_matrix(self.y_test,self.predizione)
-            cross_validation = cross_val_score(DecisionTreeClassifier(),self.X_train,self.y_train,cv=5)
+            self.report = classification_report(self.y_test,self.predizione)
+            self.matrice = confusion_matrix(self.y_test,self.predizione)
+            self.cross_validation = cross_val_score(DecisionTreeClassifier(),self.X_train,self.y_train,cv=5)
 
-            print(f"Classification report:\n {report}")
-            print(f"\nMatrice di confusione:\n {matrice}")
-            print(f"\nCross validation:\n {cross_validation}")
-
-
-            return report, matrice
+            return self.report, self.matrice
         except:
             print("Non hai ancora eseguito il modello!")
+
+    def visualizzazione_dati(self):
+
+
+        df = pd.DataFrame(data=self.X, columns=self.data.feature_names)
+        df['target'] = self.y
+
+        print(f"Visualizzo le prime 5 righe del dataset Wine:\n {df.head()}")
+
+
+        print(f"\nL'accuratezza del modello è {self.accuracy}")
+        print(f"\nL'errore quadratico medio del modello è {self.mse}")
+
+        print(f"Classification report:\n {self.report}")
+        print(f"\nMatrice di confusione:\n {self.matrice}")
+        print(f"\nCross validation:\n {self.cross_validation}")
+
 
 modello = ModelloWine()
 modello.caricamento_dati()

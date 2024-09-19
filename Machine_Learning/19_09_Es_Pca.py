d7bec34693592f88f58a60047f0c80894445bec8
nicklave <nicola.lavecchia43@gmail.com>
2024-09-19 17:57:09 +0200

    Update 19_09_Es_Pca.py

--- Machine_Learning/19_09_Es_Pca.py
+++ Machine_Learning/19_09_Es_Pca.py
@@ -37,7 +37,7 @@ print(f"Percentuale di varianza spiegata dalla quarta componente: {varianza_spie
 
 # Visualizza l'andamento della varianza spiegata cumulativa
 # varianza_cumulata = np.cumsum(varianza_spiegata)
-print(f"Varianza cumulata spiegata dalle prime 4 componenti: {var_totale * 100}")
+print(f"Varianza cumulata spiegata dalle prime 4 componenti: {var_totale * 100:.2f}%")
 
 plt.figure(figsize=(8, 6))
 scatter = plt.scatter(X_pca[:, 0], X_pca[:, 1], c=y, cmap='tab10', s=50, edgecolor='k')
@@ -58,17 +58,22 @@ accuracy_original = accuracy_score(y_test, y_pred)
 print(f"Accuratezza con dati originali (64 dimensioni): {accuracy_original:.2f}")
 
 
-X_train_pca, X_test_pca, _, _ = train_test_split(X_pca, y, test_size=0.3, random_state=42)
+# Trasforma i dati di training e test con PCA a 2 componenti
+X_train_pca, X_test_pca, y_train_pca, y_test_pca = train_test_split(X_pca, y, test_size=0.3, random_state=42)
+
+# Addestra e valuta il modello sui dati ridotti a 2 componenti
 model_pca = LogisticRegression(max_iter=10000)
-model_pca.fit(X_train_pca, y_train)
+model_pca.fit(X_train_pca, y_train_pca)
 y_pred_pca = model_pca.predict(X_test_pca)
-accuracy_pca = accuracy_score(y_test, y_pred_pca)
+accuracy_pca = accuracy_score(y_test_pca, y_pred_pca)
 print(f"Accuratezza con dati ridotti (PCA - 2 dimensioni): {accuracy_pca:.2f}")
 
+# Trasforma i dati di training e test con PCA a 4 componenti
+X_train_pca2, X_test_pca2, y_train_pca2, y_test_pca2 = train_test_split(X_pca2, y, test_size=0.3, random_state=42)
 
-X_train_pca2, X_test_pca2, _, _ = train_test_split(X_pca2, y, test_size=0.3, random_state=42)
+# Addestra e valuta il modello sui dati ridotti a 4 componenti
 model_pca2 = LogisticRegression(max_iter=10000)
-model_pca2.fit(X_train_pca2, y_train)
-y_pred_pca2 = model_pca.predict(X_test_pca2)
-accuracy_pca2 = accuracy_score(y_test, y_pred_pca2)
-print(f"Accuratezza con dati ridotti (PCA - 4 dimensioni): {accuracy_pca:.2f}")
+model_pca2.fit(X_train_pca2, y_train_pca2)
+y_pred_pca2 = model_pca2.predict(X_test_pca2)
+accuracy_pca2 = accuracy_score(y_test_pca2, y_pred_pca2)
+print(f"Accuratezza con dati ridotti (PCA - 4 dimensioni): {accuracy_pca2:.2f}")
